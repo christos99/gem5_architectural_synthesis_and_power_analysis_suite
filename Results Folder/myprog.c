c4d850c555ea9acdc44376d561a1fa358fe736ac
christos99 <mangas.99@windowslive.com>
2020-11-23 18:18:26 +0200

    Add files via upload

---
+++ Results Folder/myprog.c
@@ -0,0 +1,12 @@
+		//The c code 
+		//A simple progrma that prints a "A sample C program" output
+		#include<stdio.h>
+		
+		int main()
+		{
+			printf("\nA sample C program\n\n");
+		return 0;
+		
+		}
+		//end
+
